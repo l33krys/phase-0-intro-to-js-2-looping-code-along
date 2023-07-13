b4c14c196d281068392ed27815924fb723c2c105
Krystle Lee <l33.krys@gmail.com>
2023-07-13 16:33:02 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(arr, event) {
+    const messages = [];
+    for (let i = 0; i < arr.length; i++) {
+        messages[i] = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+        debugger;
+    }
+
+    return messages;
+}
+
+let names = ['amy', 'bob', 'carl'];
+writeCards(names, "birthday");
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
